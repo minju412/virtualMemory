eace80f062d06254e7509f022ea74c6445b679da
minju412 <ln8847@ajou.ac.kr>
2021-06-11 18:05:56 +0900

    pa3

--- pa3.c
+++ pa3.c
@@ -76,39 +76,15 @@ void push_stack(int num)
 	INIT_LIST_HEAD(&node->list);
 	list_add_tail(&node->list, &stack);
 }
-int find_min(void){
-    //min을 찾아서 리턴하고 삭제
-    struct list_head *ptr, *ptrn;
-	struct entry *node;
-    int min=1000;
-   
-   //min 찾기
-    list_for_each_prev(ptr, &stack){ 
-		node =list_entry(ptr, struct entry, list);
-		if(node->num < min)
-            min = node->num;
-	}
-
-	list_for_each_safe(ptr, ptrn, &stack){ 
-		node =list_entry(ptr, struct entry, list);
-		if(node->num == min){
-            list_del(&node->list);
-            // free(node);
-            return min;
-        }
-	}
-}
 
 int cnt=-1;
 int global_pd_index=0; 
 int mapcnt_index=0; 
 
-struct pte *pte;; ////////////////////////////추가
-int pte_flag=0; ////////////////////////////추가
+struct pte *pte;
 
 unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사용?, 0 ~ 15
 { 
-    // printf("start alloc!\n");
     int pd_index = vpn / NR_PTES_PER_PAGE; //outer의 인덱스
 	int pte_index = vpn % NR_PTES_PER_PAGE; //ptes의 인덱스
 
@@ -123,8 +99,6 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
 
     pte = &current->pagetable.outer_ptes[pd_index]->ptes[pte_index];
 
-   
-
     if(rw == 1){ //rw == RW_READ
 		pte->valid = true;
 		pte->writable = false;
@@ -135,20 +109,10 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
         pte->private = 1; //추가 - write 였음
 	}
 
-
-     /////////////////////////추가
-//    if(pte_flag == 1){ //freed MMU
-//         printf("flag=1!!\n");
-//         pte->valid = false;
-//         pte_flag=0; //돌려 놓기.. 
-//               ////???
-//    }
-
-
    if(!list_empty(&stack)){ //스택이 비어있지 않다면
 
         //스택을 돌면서 mapcounts[]가 0인게 있다면 그거 사용 -> 맵카운트 0인게 많으면 그 중 가장 작은거 사용!!!
-        //min을 찾아서 리턴하고 삭제
+       
         struct list_head *ptr, *ptrn;
         struct entry *node;
         int ret=-1;
@@ -163,11 +127,9 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
             node =list_entry(ptr, struct entry, list);
             if(node->num == ret){
                 list_del(&node->list);
-                // free(node);
-                // return ret;
             }
         }
-        // int min = find_min();
+       
         pte->pfn = ret;
         mapcounts[ret]++;
         mapcnt_index++;
@@ -181,11 +143,9 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
     pte->pfn = cnt;
     mapcounts[cnt]++;
     mapcnt_index++;
-    // printf("1 : %d\n", cnt);
+    
     return cnt;
     
-
-	// return -1;
 }
 
 
@@ -200,26 +160,11 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
  */
 void free_page(unsigned int vpn) //맵카운트가 0일때는 free하고 0보다 크면 아예 반환해버리면 안됨
 {
-    // printf("start free!\n");
+    //Deallocate the page at VPN @vpn -> vpn에 대한 페이지를 떼어내기!!
 	int pd_index = vpn / NR_PTES_PER_PAGE; //outer의 인덱스
 	int pte_index = vpn % NR_PTES_PER_PAGE; //ptes의 인덱스
 
     pte = &current->pagetable.outer_ptes[pd_index]->ptes[pte_index];
-
-
-
-
-
-    //원래 read였는지 write였는지에 따라 다르게 cow 적용!
-    // if(pte->private == 0){ //원래 read만
-    // }
-    // else{ //원래 read,write
-
-    // }
-
-
-
-    //Deallocate the page at VPN @vpn -> vpn에 대한 페이지를 떼어내기!!
 
     mapcounts[pte->pfn]--;
 	mapcnt_index--;
@@ -230,50 +175,6 @@ void free_page(unsigned int vpn) //맵카운트가 0일때는 free하고 0보다
     pte->pfn = 0;
     pte->valid = false;
     pte->writable = false;
-
-   
-
-    // if(mapcounts[tmp] == 0){ //혼자 쓰고 있던 것  
-	// 	// freed VPN should be denied by MMU!!!!!!!!!!
-	// 	printf("alone!\n");
-    //     // pte_flag = 1;       
-    // }
-	
-
-    
-    
-    // else{ //두개 이상의 프로세스와 공유하던 page
-    //     printf("more two\n");
-    // }
-
-	// if(mapcounts[pte->pfn] >= 1){ //다른 프로세스도 같이 쓰고 있던 페이지
-    //     push_stack(pte->pfn);  /////////////??
-    // } else{ //나 혼자 쓰고 있었다면 스택에서 찾아서 지우기?
-	// 	struct list_head *ptr, *ptrn;
-	// 	struct entry *node;
-	// 	int flag=0;
-	
-	// 	list_for_each_prev(ptr, &stack){ 
-	// 		node =list_entry(ptr, struct entry, list);			
-	// 		if(node->num == pte->pfn){ //존재하면 삭제
-	// 			flag=1;
-	// 		}
-	// 	}
-	// 	if(flag==1){
-	// 		list_for_each_safe(ptr, ptrn, &stack){ 
-	// 			node =list_entry(ptr, struct entry, list);
-	// 			if(node->num == pte->pfn){
-	// 				list_del(&node->list);					
-	// 			}
-	// 		}
-	// 	}
-		
-	// }
-
-    // // push_stack(pte->pfn);
-    // pte->pfn = 0;
-	// pte->valid = false;
-	// pte->writable = false;
     
 }
 
@@ -296,7 +197,6 @@ void free_page(unsigned int vpn) //맵카운트가 0일때는 free하고 0보다
  */
 bool handle_page_fault(unsigned int vpn, unsigned int rw) //상태가 Invalid일 때(page fault) MMU가 운영체제에게 부탁, copy-on-write 구현
 {
-    // printf("start handling!\n");
     int pd_index = vpn / NR_PTES_PER_PAGE; //outer의 인덱스
 	int pte_index = vpn % NR_PTES_PER_PAGE; //ptes의 인덱스
     struct pte_directory *pd = current->pagetable.outer_ptes[pd_index];
@@ -311,79 +211,20 @@ bool handle_page_fault(unsigned int vpn, unsigned int rw) //상태가 Invalid일
 	}else if(pte->private == 1){ //원래 read write 였음
         //내용을 다른 page frame에 copy한 뒤에
 
-        //현재 캡카운트가 1이면 new_pfn 찾지 말고 그냥 자기껄로 만들기! ///////////추가
+        //현재 캡카운트가 1이면 new_pfn 찾지 말고 그냥 자기껄로 만들기! 
         if(mapcounts[old_pfn]==1){
             pte->writable = true;
         }else{
 
             //new_pfn 찾기
-            // if(list_empty(&stack)){
-                cnt++;
-                new_pfn = cnt;         
-            // }else{ //free 받은 pfn이 있다면
-            //     int min = find_min();
-            //     new_pfn = min;       
-            // }
-
-
-
-            // if(!list_empty(&stack)){ //스택이 비어있지 않다면
-
-            //     //스택을 돌면서 mapcounts[]가 0인게 있다면 그거 사용
-            //     //min을 찾아서 리턴하고 삭제
-            //     struct list_head *ptr, *ptrn;
-            //     struct entry *node;
-            //     int ret=-1;
+            cnt++;
+            new_pfn = cnt;         
             
-            //     //ret 찾기
-            //     list_for_each_prev(ptr, &stack){ 
-            //         node =list_entry(ptr, struct entry, list);
-            //         if(mapcounts[node->num]==0)
-            //             ret = node->num;
-            //     }
-            //     list_for_each_safe(ptr, ptrn, &stack){ 
-            //         node =list_entry(ptr, struct entry, list);
-            //         if(node->num == ret){
-            //             list_del(&node->list);
-            //             // free(node);
-            //             // return ret;
-            //         }
-            //     }
-            //     // int min = find_min();
-            //     // pte->pfn = ret;
-            //     // mapcounts[ret]++;
-            //     // mapcnt_index++;
-
-            //     if(ret >= 0) //스택 비어있지 않아도 ret을 못 찾을 수 있음..!!
-            //         new_pfn = ret;
-            // }
-
-            // else{//스택이 비어있거나 맵카운트==0 인게 없다면
-            //     cnt++;
-            //     // pte->pfn = cnt;
-            //     // mapcounts[cnt]++;
-            //     // mapcnt_index++;
-            //     // printf("1 : %d\n", cnt);
-            //     new_pfn = cnt;
-            // }
-
-
-
-
-
-
-
-
-
-
-            
-            //원래 매핑을 끊고 (free 아닌듯!) 
+            //원래 매핑을 끊고 copy한 frame에 연결하고 
             pte->pfn = new_pfn;
-            //copy한 frame에 연결하고 
-            // current->pagetable.outer_ptes[pd_index]->ptes[new_pfn].pfn = new_pfn;
+            
             //w를 켜주고 (PTE update)
             pte->writable = true;
-            // current->pagetable.outer_ptes[pd_index]->ptes[new_pfn].writable = true;
             
             mapcounts[old_pfn]--;
             mapcounts[new_pfn]++;
@@ -415,18 +256,15 @@ struct process child;
 void switch_process(unsigned int pid) 
 //존재하는 프로세스면 context switch(ptbr을 바꿔줘야함), 존재하지 않으면 fork
 {
-    // printf("start switch!\n");
     struct list_head* ptr;
     struct process* prc = NULL;
     int flag=0;
-   
-    // printf("1. current=%d\n", current->pid);
 
     list_for_each(ptr, &processes){
         prc = list_entry(ptr, struct process, list);
 
         if(prc->pid == pid){ //존재하는 프로세스로 switch
-            // printf("find!\n");
+            
             flag=1;
             
             list_del(&prc->list);
@@ -440,14 +278,7 @@ void switch_process(unsigned int pid)
     
 here:
     if(flag==0){ //존재하지 않는 프로세스로 switch
-        // printf("forked!\n");
         //깊은 복사 -> 포인터를 복사하는게 아니라 하나하나 내용 복사!
-        // printf("2. current=%d\n", current->pid);
-        // child.pid = pid;
-        // printf("3. current=%d\n", current->pid);
-        
-        // printf("current=%d child=%d\n", current->pid, child.pid);
-
 
         //copy-on-write
         //current의 w를 끄기 (원래 read였으면 pte->private=0 / write였으면 private=1)
@@ -457,17 +288,7 @@ here:
             }
         } 
 
-        //mapcount 추가 -> 이렇게 하면 안될듯!
-        // for(int i=0; i<mapcnt_index; i++)
-        //     mapcounts[i]++;
-
-        //valid한 pte를 찾아서
-        //mapcounts[pte->pfn]++;
-
-
-
-        
-        // printf("4. current=%d\n", current->pid);
+   
         for(int i=0; i<=global_pd_index; i++){ //current의 outertable이 몇개까지 있는지!!!
             if(!child.pagetable.outer_ptes[i])
                 child.pagetable.outer_ptes[i] = malloc(sizeof(struct pte_directory));
--- vm_14.c
+++ vm_14.c
@@ -84,39 +84,37 @@ void push_stack(int num)
 	INIT_LIST_HEAD(&node->list);
 	list_add_tail(&node->list, &stack);
 }
-int find_min(void){
-    //min을 찾아서 리턴하고 삭제
-    struct list_head *ptr, *ptrn;
-	struct entry *node;
-    int min=1000;
+// int find_min(void){
+//     //min을 찾아서 리턴하고 삭제
+//     struct list_head *ptr, *ptrn;
+// 	struct entry *node;
+//     int min=1000;
    
-   //min 찾기
-    list_for_each_prev(ptr, &stack){ 
-		node =list_entry(ptr, struct entry, list);
-		if(node->num < min)
-            min = node->num;
-	}
+//    //min 찾기
+//     list_for_each_prev(ptr, &stack){ 
+// 		node =list_entry(ptr, struct entry, list);
+// 		if(node->num < min)
+//             min = node->num;
+// 	}
 
-	list_for_each_safe(ptr, ptrn, &stack){ 
-		node =list_entry(ptr, struct entry, list);
-		if(node->num == min){
-            list_del(&node->list);
-            // free(node);
-            return min;
-        }
-	}
-}
+// 	list_for_each_safe(ptr, ptrn, &stack){ 
+// 		node =list_entry(ptr, struct entry, list);
+// 		if(node->num == min){
+//             list_del(&node->list);
+//             // free(node);
+//             return min;
+//         }
+// 	}
+// }
 
 int cnt=-1;
 int global_pd_index=0; 
 int mapcnt_index=0; 
 
-struct pte *pte;; ////////////////////////////추가
-int pte_flag=0; ////////////////////////////추가
+struct pte *pte;
 
 unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사용?, 0 ~ 15
-{ 
-    // printf("start alloc!\n");
+{     
     int pd_index = vpn / NR_PTES_PER_PAGE; //outer의 인덱스
 	int pte_index = vpn % NR_PTES_PER_PAGE; //ptes의 인덱스
 
@@ -129,9 +127,7 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
        current->pagetable.outer_ptes[pd_index] = malloc(sizeof(struct pte_directory));
    }
 
-    pte = &current->pagetable.outer_ptes[pd_index]->ptes[pte_index];
-
-   
+    pte = &current->pagetable.outer_ptes[pd_index]->ptes[pte_index];  
 
     if(rw == 1){ //rw == RW_READ
 		pte->valid = true;
@@ -143,14 +139,6 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
         pte->private = 1; //추가 - write 였음
 	}
 
-
-     /////////////////////////추가
-//    if(pte_flag == 1){ //freed MMU
-//         printf("flag=1!!\n");
-//         pte->valid = false;
-//         pte_flag=0; //돌려 놓기.. 
-//               ////???
-//    }
 
 
    if(!list_empty(&stack)){ //스택이 비어있지 않다면
@@ -171,11 +159,10 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
             node =list_entry(ptr, struct entry, list);
             if(node->num == ret){
                 list_del(&node->list);
-                // free(node);
-                // return ret;
+                
             }
         }
-        // int min = find_min();
+        
         pte->pfn = ret;
         mapcounts[ret]++;
         mapcnt_index++;
@@ -192,32 +179,15 @@ unsigned int alloc_page(unsigned int vpn, unsigned int rw) //vpn을 index로 사
     // printf("1 : %d\n", cnt);
     return cnt;
     
-
-	// return -1;
 }
 
 void free_page(unsigned int vpn) //맵카운트가 0일때는 free하고 0보다 크면 아예 반환해버리면 안됨
 {
-    // printf("start free!\n");
+    //Deallocate the page at VPN @vpn -> vpn에 대한 페이지를 떼어내기!!
 	int pd_index = vpn / NR_PTES_PER_PAGE; //outer의 인덱스
 	int pte_index = vpn % NR_PTES_PER_PAGE; //ptes의 인덱스
 
     pte = &current->pagetable.outer_ptes[pd_index]->ptes[pte_index];
-
-
-
-
-
-    //원래 read였는지 write였는지에 따라 다르게 cow 적용!
-    // if(pte->private == 0){ //원래 read만
-    // }
-    // else{ //원래 read,write
-
-    // }
-
-
-
-    //Deallocate the page at VPN @vpn -> vpn에 대한 페이지를 떼어내기!!
 
     mapcounts[pte->pfn]--;
 	mapcnt_index--;
@@ -229,49 +199,6 @@ void free_page(unsigned int vpn) //맵카운트가 0일때는 free하고 0보다
     pte->valid = false;
     pte->writable = false;
 
-   
-
-    // if(mapcounts[tmp] == 0){ //혼자 쓰고 있던 것  
-	// 	// freed VPN should be denied by MMU!!!!!!!!!!
-	// 	printf("alone!\n");
-    //     // pte_flag = 1;       
-    // }
-	
-
-    
-    
-    // else{ //두개 이상의 프로세스와 공유하던 page
-    //     printf("more two\n");
-    // }
-
-	// if(mapcounts[pte->pfn] >= 1){ //다른 프로세스도 같이 쓰고 있던 페이지
-    //     push_stack(pte->pfn);  /////////////??
-    // } else{ //나 혼자 쓰고 있었다면 스택에서 찾아서 지우기?
-	// 	struct list_head *ptr, *ptrn;
-	// 	struct entry *node;
-	// 	int flag=0;
-	
-	// 	list_for_each_prev(ptr, &stack){ 
-	// 		node =list_entry(ptr, struct entry, list);			
-	// 		if(node->num == pte->pfn){ //존재하면 삭제
-	// 			flag=1;
-	// 		}
-	// 	}
-	// 	if(flag==1){
-	// 		list_for_each_safe(ptr, ptrn, &stack){ 
-	// 			node =list_entry(ptr, struct entry, list);
-	// 			if(node->num == pte->pfn){
-	// 				list_del(&node->list);					
-	// 			}
-	// 		}
-	// 	}
-		
-	// }
-
-    // // push_stack(pte->pfn);
-    // pte->pfn = 0;
-	// pte->valid = false;
-	// pte->writable = false;
     
 }
 
@@ -292,79 +219,20 @@ bool handle_page_fault(unsigned int vpn, unsigned int rw) //상태가 Invalid일
 	}else if(pte->private == 1){ //원래 read write 였음
         //내용을 다른 page frame에 copy한 뒤에
 
-        //현재 캡카운트가 1이면 new_pfn 찾지 말고 그냥 자기껄로 만들기! ///////////추가
+        //현재 캡카운트가 1이면 new_pfn 찾지 말고 그냥 자기껄로 만들기! 
         if(mapcounts[old_pfn]==1){
             pte->writable = true;
         }else{
 
-            //new_pfn 찾기
-            // if(list_empty(&stack)){
-                cnt++;
-                new_pfn = cnt;         
-            // }else{ //free 받은 pfn이 있다면
-            //     int min = find_min();
-            //     new_pfn = min;       
-            // }
-
-
-
-            // if(!list_empty(&stack)){ //스택이 비어있지 않다면
-
-            //     //스택을 돌면서 mapcounts[]가 0인게 있다면 그거 사용
-            //     //min을 찾아서 리턴하고 삭제
-            //     struct list_head *ptr, *ptrn;
-            //     struct entry *node;
-            //     int ret=-1;
+            //new_pfn 찾기        
+            cnt++;
+            new_pfn = cnt;         
             
-            //     //ret 찾기
-            //     list_for_each_prev(ptr, &stack){ 
-            //         node =list_entry(ptr, struct entry, list);
-            //         if(mapcounts[node->num]==0)
-            //             ret = node->num;
-            //     }
-            //     list_for_each_safe(ptr, ptrn, &stack){ 
-            //         node =list_entry(ptr, struct entry, list);
-            //         if(node->num == ret){
-            //             list_del(&node->list);
-            //             // free(node);
-            //             // return ret;
-            //         }
-            //     }
-            //     // int min = find_min();
-            //     // pte->pfn = ret;
-            //     // mapcounts[ret]++;
-            //     // mapcnt_index++;
-
-            //     if(ret >= 0) //스택 비어있지 않아도 ret을 못 찾을 수 있음..!!
-            //         new_pfn = ret;
-            // }
-
-            // else{//스택이 비어있거나 맵카운트==0 인게 없다면
-            //     cnt++;
-            //     // pte->pfn = cnt;
-            //     // mapcounts[cnt]++;
-            //     // mapcnt_index++;
-            //     // printf("1 : %d\n", cnt);
-            //     new_pfn = cnt;
-            // }
-
-
-
-
-
-
-
-
-
-
-            
-            //원래 매핑을 끊고 (free 아닌듯!) 
+            //원래 매핑을 끊고 copy한 frame에 연결하고
             pte->pfn = new_pfn;
-            //copy한 frame에 연결하고 
-            // current->pagetable.outer_ptes[pd_index]->ptes[new_pfn].pfn = new_pfn;
+           
             //w를 켜주고 (PTE update)
-            pte->writable = true;
-            // current->pagetable.outer_ptes[pd_index]->ptes[new_pfn].writable = true;
+            pte->writable = true;            
             
             mapcounts[old_pfn]--;
             mapcounts[new_pfn]++;
@@ -381,18 +249,15 @@ struct process child;
 void switch_process(unsigned int pid) 
 //존재하는 프로세스면 context switch(ptbr을 바꿔줘야함), 존재하지 않으면 fork
 {
-    // printf("start switch!\n");
     struct list_head* ptr;
     struct process* prc = NULL;
-    int flag=0;
-   
-    // printf("1. current=%d\n", current->pid);
+    int flag=0;   
 
     list_for_each(ptr, &processes){
         prc = list_entry(ptr, struct process, list);
 
         if(prc->pid == pid){ //존재하는 프로세스로 switch
-            // printf("find!\n");
+            
             flag=1;
             
             list_del(&prc->list);
@@ -406,14 +271,8 @@ void switch_process(unsigned int pid)
     
 here:
     if(flag==0){ //존재하지 않는 프로세스로 switch
-        // printf("forked!\n");
-        //깊은 복사 -> 포인터를 복사하는게 아니라 하나하나 내용 복사!
-        // printf("2. current=%d\n", current->pid);
-        // child.pid = pid;
-        // printf("3. current=%d\n", current->pid);
         
-        // printf("current=%d child=%d\n", current->pid, child.pid);
-
+        //깊은 복사 -> 포인터를 복사하는게 아니라 하나하나 내용 복사!
 
         //copy-on-write
         //current의 w를 끄기 (원래 read였으면 pte->private=0 / write였으면 private=1)
@@ -423,17 +282,7 @@ here:
             }
         } 
 
-        //mapcount 추가 -> 이렇게 하면 안될듯!
-        // for(int i=0; i<mapcnt_index; i++)
-        //     mapcounts[i]++;
-
-        //valid한 pte를 찾아서
-        //mapcounts[pte->pfn]++;
-
-
-
         
-        // printf("4. current=%d\n", current->pid);
         for(int i=0; i<=global_pd_index; i++){ //current의 outertable이 몇개까지 있는지!!!
             if(!child.pagetable.outer_ptes[i])
                 child.pagetable.outer_ptes[i] = malloc(sizeof(struct pte_directory));
@@ -450,12 +299,12 @@ here:
                 }                  
             }
         }
-        // printf("5. current=%d\n", current->pid);
+        
         list_add_tail(&current->list, &processes);
-        child.pid = pid; ///////////
+        child.pid = pid; 
         ptbr = &child.pagetable;
         current = &child;
-        // printf("6. current=%d\n", current->pid);
+      
     }
     
 }
